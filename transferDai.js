690186358fd65b381fc540e263886b4b17202945
LAPTOP-BMG44977\migue <miguel567@gmail.com>
2020-06-14 18:09:16 +0200

    trying transfer DAI

---
+++ transferDai.js
@@ -0,0 +1,35 @@
+let tokenAddress = process.env.DAI_ADDRESS;
+let walletAddress = process.env.ACCOUNT_ADDRESS;
+
+// The minimum ABI to get ERC20 Token balance
+let minABI = [
+  // balanceOf
+  {
+    "constant":true,
+    "inputs":[{"name":"_owner","type":"address"}],
+    "name":"balanceOf",
+    "outputs":[{"name":"balance","type":"uint256"}],
+    "type":"function"
+  },
+  // decimals
+  {
+    "constant":true,
+    "inputs":[],
+    "name":"decimals",
+    "outputs":[{"name":"","type":"uint8"}],
+    "type":"function"
+  }
+];
+
+// Get ERC20 Token contract instance
+let contract = web3.eth.contract(minABI).at(tokenAddress);
+
+// Call balanceOf function
+contract.balanceOf(walletAddress, (error, balance) => {
+  // Get decimals
+  contract.decimals((error, decimals) => {
+    // calculate a balance
+    balance = balance.div(10**decimals);
+    console.log(balance.toString());
+  });
+});
